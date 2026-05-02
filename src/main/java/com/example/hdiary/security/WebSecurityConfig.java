dd4d521948f1074a2d4db30acf2012586b6edde9
Kiryl Matsiazh <kirillmatazsh@gmail.com>
2026-05-02 17:14:06 +0200

    - Implement password hashing.

--- src/main/java/com/example/hdiary/security/WebSecurityConfig.java
+++ src/main/java/com/example/hdiary/security/WebSecurityConfig.java
@@ -1,18 +1,17 @@
 package com.example.hdiary.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 @Configuration
 public class WebSecurityConfig {
-    @Autowired
-    HDiaryUserDetailsService userDetailsService;
     @Bean
     public AuthTokenFilter authenticationJwtTokenFilter() {
         return new AuthTokenFilter();
@@ -22,6 +21,11 @@ public class WebSecurityConfig {
             AuthenticationConfiguration authenticationConfiguration
     ) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    // Using PasswordEncoder type not to depend on BCryptPasswordEncoder implementation.
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder(12);
     }
 
 
--- src/main/java/com/example/hdiary/service/AuthService.java
+++ src/main/java/com/example/hdiary/service/AuthService.java
@@ -4,27 +4,34 @@ import com.example.hdiary.model.HDiaryUser;
 import com.example.hdiary.model.Sex;
 import com.example.hdiary.repository.HDiaryUserRepository;
 import com.example.hdiary.security.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
 @Service
 public class AuthService {
     private final HDiaryUserRepository userRepository;
     private final HDiaryUserService userService;
     private final JwtUtil jwtUtil;
+    private PasswordEncoder encoder;
 
-    public AuthService(HDiaryUserRepository userRepository, HDiaryUserService userService, JwtUtil jwtUtil){
+    public AuthService(
+            HDiaryUserRepository userRepository,
+            HDiaryUserService userService,
+            JwtUtil jwtUtil,
+            PasswordEncoder encoder
+    ){
         this.userRepository = userRepository;
         this.userService = userService;
         this.jwtUtil = jwtUtil;
+        this.encoder = encoder;
     }
 
 
     /*
     TODO ===> Validation for each field is required;
-    TODO ===> Password hashing is required;
     TODO ===> Already used username and email verification is required;
     TODO ===> Password strength verification is required;
     */
@@ -40,20 +47,19 @@ public class AuthService {
                 dateOfBirth,
                 sex,
                 email,
-                password
+                encoder.encode(password)
         );
         return userRepository.save(user);
     }
 
     /*
-    TODO ===> Password comparison will require changes after hashing implementation;
     TODO ===> Transfer object implementation is required not to send the whole entity with sensitive data;
     TODO ===> Proper exceptions and their handling is required;
     */
     public String logIn(String email, String password){
         HDiaryUser user = userService.findByEmail(email);
 
-        if(user == null || !Objects.equals(password, user.getPassword())){
+        if(user == null || !encoder.matches(password, user.getPassword())){
             return null;
         }
 
