e60969c663e58651eae75b6210d35a0781641ff6
Kiryl Matsiazh <kirillmatazsh@gmail.com>
2026-07-18 11:53:57 +0200

    - Add /user/me controller.

--- src/main/java/com/example/hdiary/controller/HDiaryUserController.java
+++ src/main/java/com/example/hdiary/controller/HDiaryUserController.java
@@ -1,6 +1,9 @@
 package com.example.hdiary.controller;
 
+import com.example.hdiary.dto.response.LoginHDiaryUserResponseDTO;
+import com.example.hdiary.model.HDiaryUser;
 import com.example.hdiary.service.HDiaryUserService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,4 +15,16 @@ public class HDiaryUserController {
         this.hDiaryUserService = hDiaryUserService;
     }
 
+    @GetMapping("/me")
+    public LoginHDiaryUserResponseDTO getMe(){
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        HDiaryUser user = hDiaryUserService.findByEmail(email);
+
+        return new LoginHDiaryUserResponseDTO(
+                user.getUsername(),
+                user.getDateOfBirth(),
+                user.getSex(),
+                user.getEmail()
+        );
+    }
 }
